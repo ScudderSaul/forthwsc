093222179fe477739892e35e1432dab710b4919f
Saul Scudder <Saul.Scudder@gmail.com>
2022-08-24 12:47:30 -0700

    Update codeelement.cpp

--- codeelement.cpp
+++ codeelement.cpp
@@ -1,12 +1,27 @@
 //+---------------------------------------------------------------------------------------+
-//This file codeelement.cpp is copyright 1999-2008 by Saul Scudder
-//It is distributed for non-profit use as freeware and no warranty is given with the use of
-//this source for any purpose whatsoever.
-//This copyright notice is to remain unmodified in the file
-//_Zen_Soft c/o 
-//Saul Scudder
-//358 E. Whitton
-//Phoenix AZ 85012
+// Copyright(c) 1999-2022 by Saul Scudder
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files(the "Software"), to deal in the Software without restriction, 
+//including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+//and /or sell copies of the Software, and to permit persons to whom the Software is 
+//furnished to do so, subject to the following conditions:
+
+//The above copyright noticeand this permission notice shall be included in 
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+//INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
+//AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+//DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// a donation to Saul Scudder
+// the programer (now retired) who wrote this version of Forth
+// may be sent to me (if you want to) 
+// Saul Scudder
+// 233 W. Erie St.
+// Holbrook AZ 86025
 //+---------------------------------------------------------------------------------------+
 
 #include "stdafx.h"
